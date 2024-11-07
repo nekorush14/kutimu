4e448d98af781794c4a021f2426eed0ea4dbe767
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2024-11-07 23:19:33 +0900

    Add: #1 Create data class for habit and task.

---
+++ app/src/main/java/dev/nekorush14/kutimu/data/KutimuEnums.kt
@@ -0,0 +1,29 @@
+package dev.nekorush14.kutimu.data
+
+/**
+ *  An enum class to define category of tasks.
+ */
+enum class TaskCategory {
+    Inbox, Private, Work
+}
+
+/**
+ *  An enum class to define frequency of tasks.
+ */
+enum class TaskFrequency {
+    Once, Daily, Weekly, Monthly, Yearly
+}
+
+/**
+ *  An enum class to define category of habits.
+ */
+enum class HabitCategory {
+    Inbox, Task, Hobby, Important
+}
+
+/**
+ *  An enum class to define frequency of habits.
+ */
+enum class HabitFrequency {
+    Daily, Weekly, Monthly, Yearly
+}
