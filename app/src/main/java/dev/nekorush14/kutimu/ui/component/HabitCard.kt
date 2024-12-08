8889f9c15a9fdcf54427e132b7bc642df885941c
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2024-12-08 11:46:54 +0900

    Add: #7 Create all habit screen.
    * Additional actions
      * Toggle pined status to show home screen

--- app/src/main/java/dev/nekorush14/kutimu/ui/component/HabitCard.kt
+++ app/src/main/java/dev/nekorush14/kutimu/ui/component/HabitCard.kt
@@ -1,18 +1,23 @@
 package dev.nekorush14.kutimu.ui.component
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Inbox
+import androidx.compose.material.icons.filled.PushPin
 import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.PushPin
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,12 +38,18 @@ import dev.nekorush14.kutimu.ui.theme.KutimuTheme
  * and completion status based on [Habit.frequency].
  *
  * @param habit Habit to be displayed.
+ * @param onHabitClick Callback to be invoked when the habit card is clicked.
+ * @param onPinClick Callback to be invoked when the pin button is clicked.
+ * @param isAllHabitScreen Flag to indicate if the habit card is displayed in the all habits screen.
  * @param frequencyMaxCount Maximum count of status icons to display.
  * @param modifier Modifier to be applied to the card.
  */
 @Composable
 fun HabitCard(
     habit: Habit,
+    onHabitClick: () -> Unit = {},
+    onPinClick: (Long) -> Unit = {},
+    isAllHabitScreen: Boolean = false,
     frequencyMaxCount: Int = 7,
     modifier: Modifier = Modifier
 ) {
@@ -53,6 +64,9 @@ fun HabitCard(
             defaultElevation = dimensionResource(R.dimen.card_elevation_default)
         ),
         modifier = modifier
+            .clickable(enabled = isAllHabitScreen) {
+                onHabitClick()
+            }
             .fillMaxWidth()
             .padding(dimensionResource(R.dimen.card_padding_small))
     ) {
@@ -78,21 +92,52 @@ fun HabitCard(
                 )
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth()
                         .padding(top = dimensionResource(R.dimen.card_padding_small))
                 ) {
+                    // Show filled icons for completed count
                     repeat(habit.completeCount) {
                         Icon(
                             imageVector = Icons.Filled.CheckCircle,
                             contentDescription = null,
                         )
                     }
+                    // Show outlined icons for remaining count
                     repeat(frequencyMaxCount - habit.completeCount) {
                         Icon(
                             imageVector = Icons.Outlined.CheckCircle,
                             contentDescription = null,
                         )
                     }
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            if (isAllHabitScreen && habit.isPined) {
+                IconButton(
+                    onClick = { onPinClick(habit.id) },
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.icon_size_medium))
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.PushPin,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(dimensionResource(R.dimen.icon_size_medium))
+                            .padding(end = dimensionResource(R.dimen.card_padding_small))
+                    )
+                }
+            } else if (isAllHabitScreen) {
+                IconButton(
+                    onClick = { onPinClick(habit.id) },
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.icon_size_medium))
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.PushPin,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(dimensionResource(R.dimen.icon_size_medium))
+                            .padding(end = dimensionResource(R.dimen.card_padding_small))
+                    )
                 }
             }
         }
@@ -113,6 +158,47 @@ private fun HabitCardPreview() {
                 frequency = HabitFrequency.Weekly,
                 category = HabitCategory.Inbox
             ),
+            frequencyMaxCount = 7
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AllHabitsScreenPinedHabitCardPreview() {
+    KutimuTheme {
+        HabitCard(
+            habit = Habit(
+                id = 1,
+                title = "Title",
+                description = "Description",
+                categoryIcon = Icons.Filled.Inbox,
+                isPined = true,
+                completeCount = 3,
+                frequency = HabitFrequency.Weekly,
+                category = HabitCategory.Inbox
+            ),
+            isAllHabitScreen = true,
+            frequencyMaxCount = 7
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AllHabitScreenUnPinedHabitCardPreview() {
+    KutimuTheme {
+        HabitCard(
+            habit = Habit(
+                id = 1,
+                title = "Title",
+                description = "Description",
+                categoryIcon = Icons.Filled.Inbox,
+                completeCount = 3,
+                frequency = HabitFrequency.Weekly,
+                category = HabitCategory.Inbox
+            ),
+            isAllHabitScreen = true,
             frequencyMaxCount = 7
         )
     }
